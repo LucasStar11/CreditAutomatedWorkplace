a3743b8c012ccbf4c5989619d7fd56409f996561
LucasStar11 <lucasstar1122@gmail.com>
2024-01-29 15:21:43 +0300

    feat: create project, setup postgresql

---
+++ src/main/java/com/example/CreditAutomatedWorkplace/CreditAutomatedWorkplaceApplication.java
@@ -0,0 +1,14 @@
+package com.example.CreditAutomatedWorkplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAutomatedWorkplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAutomatedWorkplaceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/CreditAutomatedWorkplace/CreditAutomatedWorkplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreditAutomatedWorkplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAutomatedWorkplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
